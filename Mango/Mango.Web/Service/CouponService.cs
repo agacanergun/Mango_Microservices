0645404d17cf586f5d16000e4edb18bdd11a7c44
Ağacan <agcannn@gmail.com>
2023-06-30 21:56:43 +0300

    update

--- Mango/Mango.Web/Service/CouponService.cs
+++ Mango/Mango.Web/Service/CouponService.cs
@@ -36,7 +36,7 @@ namespace Mango.Web.Service
             return await _baseService.SendAsync(new RequestDto()
             {
                 ApiType = SD.ApiType.GET,
-                Url = SD.CouponAPIBase+"/api/coupon"
+                Url = SD.CouponAPIBase+ "/api/coupon"
             });
         }
 
@@ -45,7 +45,7 @@ namespace Mango.Web.Service
             return await _baseService.SendAsync(new RequestDto()
             {
                 ApiType = SD.ApiType.GET,
-                Url = SD.CouponAPIBase + "/api/coupon/GetByCode/"+couponCode,
+                Url = SD.CouponAPIBase + "/api/coupon/GetByCode/" + couponCode,
             });
         }
 
