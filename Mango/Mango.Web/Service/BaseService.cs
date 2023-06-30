8a3a184c6a6ee7757ce7c1f4ff0c712d4f58b2ae
Ağacan <agcannn@gmail.com>
2023-06-30 20:24:15 +0300

    update

---
+++ Mango/Mango.Web/Service/BaseService.cs
@@ -0,0 +1,83 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+                HttpRequestMessage message = new HttpRequestMessage(); //mesaj oluştur
+                message.Headers.Add("Accept", "application/json"); //json kabul eder
+                //token
+
+                message.RequestUri = new Uri(requestDto.Url); //mesajın gideceği url parametredeki url
+                if (requestDto.Data != null) //parametredeki data boş değilse
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json"); //mesajın contentine parametredeki datayı json formatlayıp gönder
+
+
+                HttpResponseMessage? apiResponse = null;
+
+                //parametrenin apitipine göre mesajın method tipini belirle
+                switch (requestDto.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                //mesajı gönder ve gelen responsu tut
+                apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.Forbidden:
+                        return new() { IsSuccess = false, Message = "Access Denied" };
+                    case HttpStatusCode.Unauthorized:
+                        return new() { IsSuccess = false, Message = "Unauthorized" };
+                    case HttpStatusCode.InternalServerError:
+                        return new() { IsSuccess = false, Message = "Internal Server Error" };
+                    default:
+                        //responsedeki json contenti string yapıp apiContente atar
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        //apicontent deki veriyi ResponseDto 'ya deserialize eder
+                        var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                        return apiResponseDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto
+                {
+                    Message = ex.Message.ToString(),
+                    IsSuccess = false,
+                };
+                return dto;
+            }
+        }
+    }
+}
