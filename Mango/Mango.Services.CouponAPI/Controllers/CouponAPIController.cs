3509d97a6c25b5d7d7be3e3da6f6a92742e9f59c
Ağacan <agcannn@gmail.com>
2023-06-30 15:23:00 +0300

    update

---
+++ Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,17 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+    }
+}
