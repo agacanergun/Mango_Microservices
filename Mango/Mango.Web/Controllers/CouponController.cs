ad23e06003e9863417542d2e38b02f1997e9f731
Ağacan <agcannn@gmail.com>
2023-07-02 17:45:50 +0300

    update

--- Mango/Mango.Web/Controllers/CouponController.cs
+++ Mango/Mango.Web/Controllers/CouponController.cs
@@ -2,6 +2,7 @@
 using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Mango.Web.Controllers
 {
@@ -28,6 +29,44 @@ namespace Mango.Web.Controllers
         public async Task<IActionResult> CouponCreate()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponsAsync(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+
+            ResponseDto? response = await _couponService.GetCouponByIdAsync(couponId);
+            if (response != null && response.IsSuccess)
+            {
+                CouponDto? model = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDto couponDto)
+        {
+
+            ResponseDto? response = await _couponService.DeleteCouponsAsync(couponDto.CouponId);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CouponIndex));
+            }
+            return View(couponDto);
         }
     }
 }
